eb6494cb9b0d36428b76c8b14511ac39cf628422
nejaklah <najilaamraoui@gmail.com>
2024-03-18 15:29:52 -0700

    fixing

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-
-#!/usr/bin/python3
-import hidden_4
-if __name__ == "__main__":
-    for i in range(len(dir(hidden_4))):
-        if dir(hidden_4)[i].startswith("__"):
-            continue
-        else:
-            print(dir(hidden_4)[i])
