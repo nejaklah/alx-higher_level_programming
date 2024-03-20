f594a5ca9886edb62386d1c1fe78a6e1f8c8940c
nejaklah <najilaamraoui@gmail.com>
2024-03-20 16:20:33 -0700

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    F = []
+    TMP = []
+    for i in range(len(matrix)):
+        TMP = list(map(lambda x: x ** 2, matrix[i]))
+        F.append(TMP)
+    return F
