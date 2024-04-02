473ef9cb2f5aeb33c53d40f4796d07bfe7330132
nejaklah <najilaamraoui@gmail.com>
2024-04-02 13:41:35 -0700

    fix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        num = 0
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                num += 1
+        print("")
+        return num
+    except IndexError:
+        raise
